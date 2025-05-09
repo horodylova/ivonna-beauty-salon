b92f72317e5f21664c554d7831c5f6431431ffb3
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-05-09 18:15:22 +0100

    swiper

--- js/script.js
+++ js/script.js
@@ -144,9 +144,14 @@
     var swiper = new Swiper(".testimonial-swiper", {
       slidesPerView: 1,
       spaceBetween: 20,
+      loop: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
 
